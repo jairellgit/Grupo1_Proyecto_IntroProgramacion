93529f7d672b33477b1da3291a9e57c853eb7313
jairellgit <133938234+jairellgit@users.noreply.github.com>
2023-11-07 18:24:32 -0600

    Update Main.java

--- G1_IntroProgramacion/src/main/java/com/mycompany/g1_introprogramacion/Main.java
+++ G1_IntroProgramacion/src/main/java/com/mycompany/g1_introprogramacion/Main.java
@@ -4,17 +4,50 @@
  */
 package com.mycompany.g1_introprogramacion;
 
-/**
- *
- * @author Grupo1
- */
+import javax.swing.JOptionPane;
+
+
 public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Registro registro = new Registro();
+        Organizar organizar = new Organizar(registro);
+        Comprar comprar = new Comprar();
+        Retirar retirar = new Retirar();
+        
+        int id;
+
+        while (true) {
+            try {
+                String[] opciones = {"Registro de Productos", "Reorganizar Productos", "Realizar Compra", "Retirar Producto", "Salir"};
+                String seleccion = (String) JOptionPane.showInputDialog(null, "Seleccione una opción:", "Menú Principal", JOptionPane.PLAIN_MESSAGE, null, opciones, opciones[0]);
+
+                if (seleccion == null || seleccion.equals("Salir")) {
+                    JOptionPane.showMessageDialog(null, "Gracias por usar el sistema. ¡Hasta luego!");
+                    break;
+                } else if (seleccion.equals("Registro de Productos")) {
+                    id = registro.registrarProducto();
+                    if (id == 0){
+                        
+                    }else{
+                        JOptionPane.showMessageDialog(null, "Producto registrado con éxito. ");
+                        organizar.organizarInicial(id);
+                    }
+                } else if (seleccion.equals("Reorganizar Productos")) {
+                    int idProducto = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite el ID del producto: "));
+                    organizar.reorganizarProducto(idProducto); 
+                } else if (seleccion.equals("Realizar Compra")) { 
+                    comprar.realizarCompra();
+                } else if (seleccion.equals("Retirar Producto")) {
+                    JOptionPane.showMessageDialog(null, "Próximamente... ");
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Ha ocurrido un error. Por favor, intente de nuevo.");
+            }
+        }
     }
     
 }
