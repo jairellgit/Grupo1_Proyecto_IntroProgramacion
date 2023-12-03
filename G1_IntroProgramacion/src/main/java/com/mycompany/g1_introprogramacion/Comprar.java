72f372fd3f971d744ee40d23ec3caecf34ab4afa
HunterShiny <148643809+HunterShiny@users.noreply.github.com>
2023-12-03 09:42:13 -0600

    Update Comprar.java

--- G1_IntroProgramacion/src/main/java/com/mycompany/g1_introprogramacion/Comprar.java
+++ G1_IntroProgramacion/src/main/java/com/mycompany/g1_introprogramacion/Comprar.java
@@ -1,175 +1,184 @@
-/*
+
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.g1_introprogramacion;
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-public class Comprar {
-    //>>>EN PROGRESO
 
-    //Clases
-    Registro registro = new Registro();
-    Organizar organizar = new Organizar(registro);
+/**
+ *
+ * @author XPC
+ */
+public class Example {
+    private static final List<List<String>> productos = new ArrayList<>();
 
-    private static int totalCompras;
-    
-    //Productos (nombre)
-    private String[] productos = registro.getProductos();
-    //Productos (precio)
-    private double[] precios = registro.getPrecios();
-    //Productos (existencias)
-    private int[] existencias = registro.getExistencias();
-    //Matriz organización de los productos
-    private int[][] organizacion = organizar.getOrganizacion();
-    
-   //Arrays secundarios
-    private double[] productosComprados = new double[7];
-    private double[] productosCantidades = new double[7];
-    private double[] productosPrecio = new double[7];
-    
-    //Matriz primaria
-    private double[][] compras = new double[productosComprados.length][5];
-    
-    //Constructor
-    public Comprar() {
-
-    }
-
-    //Método principal para comprar
-    public void realizarCompra() {
-        if (totalCompras > 50) {
-            JOptionPane.showMessageDialog(null, "Se ha alcanzado el límite de compras totales.");
-        } else {
-            //Guardar la información del cliente
-            String nombreCliente = JOptionPane.showInputDialog("Nombre del cliente:");
-            int cedulaCliente = Integer.parseInt(JOptionPane.showInputDialog("Cédula del cliente:"));
-
-            int cantidadProductos = 0;
-            boolean continuarCompra = true;
-
-            while (cantidadProductos < 8 && continuarCompra) {
-                int pasillo = seleccionarPasillo();
-
-                if (pasillo == 0) {
-                    break;
-                } else {
-                    int[] seleccionarProducto = seleccionarProducto(pasillo);
-                    int idProducto = seleccionarProducto[0];
-                    int cantProducto = seleccionarProducto[1];
-                    
-                    if (idProducto ==0){
-                        JOptionPane.showMessageDialog(null, "No se añadió ningún producto a su lista.");
-                    }else{
-                        
-                    }
-                }
-                continuarCompra = JOptionPane.showConfirmDialog(null, "¿Desea comprar  otros productos?", "Continuar Comprando", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
-            }
-
-            //JOptionPane.showMessageDialog(null, "Compra realizada con éxito.");
-        }
-    }
-
-    //Elegir el pasillo
-    private int seleccionarPasillo() {
-        int pasillo = 0;
-        do {
-            try {
-                pasillo = Integer.parseInt(JOptionPane.showInputDialog("Seleccione el pasillo (1-10):\nDigite 0 para cancelar"));
-                if (pasillo == 0) {
-                    break;
-                }
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "Debe ingresar un pasillo valido. Inténtelo nuevamente.");
-            }
-        } while (pasillo < 0 || pasillo > 10);
-        return pasillo;
-    }
-
-    //Elegir el producto y la catidad
-    private int[] seleccionarProducto(int pasillo) {
-        imprimirFila(organizacion, pasillo - 1);
-
-        int idProducto = 0;
-        int cantidad = 0;
-
+    public static void main(String[] args) {
         while (true) {
+            String opcionStr;
+            opcionStr = JOptionPane.showInputDialog("""
+                                                    1. Agregar Producto
+                                                    2. Retirar Producto
+                                                    3. Mostrar Productos
+                                                    4. Salir""");
+
+            int opcion;
             try {
-                idProducto = Integer.parseInt(JOptionPane.showInputDialog("Seleccione el ID de un producto válido del pasillo " + pasillo + ":\nDigite 0 para cancelar"));
-
-                if (idProducto == 0) {
-                    break;
-                } else if (existeProducto(idProducto) == true) {
-                    cantidad = Integer.parseInt(JOptionPane.showInputDialog("Cantidad a comprar:"));
-                    return new int[]{idProducto, cantidad};
-                } else {
-                    JOptionPane.showMessageDialog(null, "El ID del producto no existe. Inténtelo nuevamente");
-                }
+                opcion = Integer.parseInt(opcionStr);
             } catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(null, "Debe ingresar un valor válido. Inténtelo nuevamente");
+                opcion = 0;
             }
-        }
-        return new int[]{idProducto, cantidad};
-    }
 
-    //Validar por el ID si el producto existe
-    private boolean existeProducto(int idProducto) {
-        int[] id = registro.getId();
-
-        for (int i = 0; i < id.length; i++) {
-            if (id[i] == idProducto) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    //Imprimir los valores del pasillo seleccionado
-    public void imprimirFila(int[][] matriz, int fila) {
-        StringBuilder filaStr = new StringBuilder();
-        for (int x = 0; x < matriz[fila].length; x++) {
-            filaStr.append(matriz[fila][x]);
-            String nombre = obtenerNombreProducto(matriz[fila][x]);
-            double precio = obtenerPrecioProducto(matriz[fila][x]);
-            filaStr.append(" - ").append(nombre).append(" - ").append(precio);
-
-            if (x < matriz[fila].length - 1) {
-                filaStr.append("\n"); // Tabulación para separar valores en la fila
-            }
-        }
-
-        String filaPrint = String.valueOf(fila + 1);
-
-        JOptionPane.showMessageDialog(null, "\nProductos en el pasillo " + filaPrint + ": \n" + filaStr.toString(), "Productos disponibles", JOptionPane.INFORMATION_MESSAGE);
-    }
-
-    //Obtener nombre del producto en base al ID
-    public String obtenerNombreProducto(int idBuscado) {
-        for (int i = 0; i < registro.getId().length; i++) {
-            if (registro.getId()[i] == idBuscado) {
-                if (idBuscado == 0) {
-                    return "No existe";
-                } else {
-                    return productos[i];
+            switch (opcion) {
+                case 1 -> agregarProducto();
+                case 2 -> retirarProducto();
+                case 3 -> mostrarProductos();
+                case 4 -> {
+                    JOptionPane.showMessageDialog(null, "Saliendo del programa. ¡Hasta luego!");
+                    System.exit(0);
                 }
+                default -> JOptionPane.showMessageDialog(null, "Opción no válida. Inténtelo de nuevo.");
             }
         }
-        return "ID no encontrado";
     }
-    
-    //Obtener nombre del producto en base al ID
-    public double obtenerPrecioProducto(int idBuscado) {
-        for (int i = 0; i < registro.getId().length; i++) {
-            if (registro.getId()[i] == idBuscado) {
-                if (idBuscado == 0) {
-                    return 0;
-                } else {
-                    return precios[i];
-                }
+
+    private static void agregarProducto() {
+        String id = JOptionPane.showInputDialog("Ingrese el ID del producto:");
+        String nombre = JOptionPane.showInputDialog("Ingrese el nombre del producto:");
+        String categoria = JOptionPane.showInputDialog("Ingrese la categoría del producto:");
+
+        List<String> nuevoProducto = new ArrayList<>();
+        nuevoProducto.add(id);
+        nuevoProducto.add(nombre);
+        nuevoProducto.add(categoria);
+
+        productos.add(nuevoProducto);
+
+        JOptionPane.showMessageDialog(null, "Producto agregado correctamente.");
+    }
+
+    private static void retirarProducto() {
+        String idRetirar = JOptionPane.showInputDialog("Ingrese el ID del producto que desea retirar:");
+
+        Iterator<List<String>> iterator = productos.iterator();
+        boolean encontrado = false;
+
+        while (iterator.hasNext()) {
+            List<String> productoActual = iterator.next();
+            if (productoActual.get(0).equals(idRetirar)) {
+                iterator.remove();
+                encontrado = true;
+                break;
             }
         }
-        return 0;
+
+        if (encontrado) {
+            JOptionPane.showMessageDialog(null, "Producto retirado correctamente.");
+        } else {
+            JOptionPane.showMessageDialog(null, "No se encontró un producto con ese ID.");
+        }
+    }
+
+    private static void mostrarProductos() {
+        StringBuilder mensaje = new StringBuilder("Lista de Productos:\n");
+
+        for (List<String> producto : productos) {
+            mensaje.append("ID: ").append(producto.get(0)).append(", Nombre: ").append(producto.get(1)).append(", Categoría: ").append(producto.get(2)).append("\n");
+        }
+
+        JOptionPane.showMessageDialog(null, mensaje.toString());
+    }
+}
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+/**
+ *
+ * @author XPC
+ */
+public class Example {
+    private static final List<List<String>> productos = new ArrayList<>();
+
+    public static void main(String[] args) {
+        while (true) {
+            String opcionStr;
+            opcionStr = JOptionPane.showInputDialog("""
+                                                    1. Agregar Producto
+                                                    2. Retirar Producto
+                                                    3. Mostrar Productos
+                                                    4. Salir""");
+
+            int opcion;
+            try {
+                opcion = Integer.parseInt(opcionStr);
+            } catch (NumberFormatException e) {
+                opcion = 0;
+            }
+
+            switch (opcion) {
+                case 1 -> agregarProducto();
+                case 2 -> retirarProducto();
+                case 3 -> mostrarProductos();
+                case 4 -> {
+                    JOptionPane.showMessageDialog(null, "Saliendo del programa. ¡Hasta luego!");
+                    System.exit(0);
+                }
+                default -> JOptionPane.showMessageDialog(null, "Opción no válida. Inténtelo de nuevo.");
+            }
+        }
+    }
+
+    private static void agregarProducto() {
+        String id = JOptionPane.showInputDialog("Ingrese el ID del producto:");
+        String nombre = JOptionPane.showInputDialog("Ingrese el nombre del producto:");
+        String categoria = JOptionPane.showInputDialog("Ingrese la categoría del producto:");
+
+        List<String> nuevoProducto = new ArrayList<>();
+        nuevoProducto.add(id);
+        nuevoProducto.add(nombre);
+        nuevoProducto.add(categoria);
+
+        productos.add(nuevoProducto);
+
+        JOptionPane.showMessageDialog(null, "Producto agregado correctamente.");
+    }
+
+    private static void retirarProducto() {
+        String idRetirar = JOptionPane.showInputDialog("Ingrese el ID del producto que desea retirar:");
+
+        Iterator<List<String>> iterator = productos.iterator();
+        boolean encontrado = false;
+
+        while (iterator.hasNext()) {
+            List<String> productoActual = iterator.next();
+            if (productoActual.get(0).equals(idRetirar)) {
+                iterator.remove();
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (encontrado) {
+            JOptionPane.showMessageDialog(null, "Producto retirado correctamente.");
+        } else {
+            JOptionPane.showMessageDialog(null, "No se encontró un producto con ese ID.");
+        }
+    }
+
+    private static void mostrarProductos() {
+        StringBuilder mensaje = new StringBuilder("Lista de Productos:\n");
+
+        for (List<String> producto : productos) {
+            mensaje.append("ID: ").append(producto.get(0)).append(", Nombre: ").append(producto.get(1)).append(", Categoría: ").append(producto.get(2)).append("\n");
+        }
+
+        JOptionPane.showMessageDialog(null, mensaje.toString());
     }
 }
